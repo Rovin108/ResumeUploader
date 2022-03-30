d29530236ec5701659608d97c2586368f03eb8d4
unknown <=>
2022-03-30 15:57:46 +0530

    first commet

---
+++ myapp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import ResumeForm
+from .models import Resume
+from django.views import View
+
+class HomeView(View):
+    def get(self, request):
+        form = ResumeForm()
+        return render(request, 'myapp/home.html', { 'form':form})
+
+    def post(self, request):
+        form = ResumeForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            candidates = Resume.objects.all()
+            return render(request, 'myapp/home.html', { 'candidates': candidates, 'form':form})
+            #return HttpResponseRedirect('myapp/templates/myapp/home.html')
+
+class CandidateView(View):
+    def get(self, request, pk):
+        candidate = Resume.objects.get(pk=pk)
+        return render(request, 'myapp/candidate.html', {'candidate':candidate})
+    
